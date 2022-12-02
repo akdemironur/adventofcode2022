c5b1f5cf910088de059376c96a5fb5668276efe7
Onur Akdemir <onur.akdemir@icloud.com>
2022-12-03 02:14:10 +0300

    Day 1 & 2 solutions

---
+++ 1/solution.hs
@@ -0,0 +1,31 @@
+import System.IO  
+import Control.Monad
+import Data.List (sort)
+
+q1 :: IO Int
+q1 = (solve 1 0 0 0) <$> readInputFile 
+
+q2 :: IO Int
+q2 =  sum.(take 3).reverse.sort.(solve2 [0]) <$> readInputFile 
+
+readInputFile = do  
+        let list = []
+        handle <- openFile "input.txt" ReadMode
+        contents <- fmap lines (hGetContents handle)
+        return contents
+
+
+solve :: Int -> Int -> Int -> Int -> [String] -> Int
+solve _ _ _ maxCal [] = maxCal
+solve currentElf currentCal maxElf maxCal (s:str)
+        | s == "" = if currentCal > maxCal 
+                    then solve (currentElf+1) 0 currentElf currentCal str 
+                    else solve (currentElf+1) 0 maxElf maxCal str
+        | otherwise = solve currentElf (currentCal + (read s)) maxElf maxCal str
+
+solve2 :: [Int] -> [String] -> [Int]
+solve2 a [] = a
+solve2 (c:cs) (s:str)
+        | s == "" = solve2 (0:c:cs) str
+        | otherwise = solve2 ((c+read s):cs) str
+
---
+++ 2/solution.hs
@@ -0,0 +1,50 @@
+import System.IO  
+import Control.Monad
+
+q1 :: IO Int
+q1 = sum . fmap pointFromRound <$> readInputFile
+
+q2 :: IO Int
+q2 = sum . fmap (pointFromRound.convertToRound) <$> readInputFile
+
+readInputFile :: IO [[String]]
+readInputFile = do  
+        let list = []
+        handle <- openFile "input.txt" ReadMode
+        contents <-  (fmap lines (hGetContents handle))
+        return (fmap words contents)
+
+pointFromPick :: [String] -> Int
+pointFromPick [_,"X"] = 1
+pointFromPick [_,"Y"] = 2
+pointFromPick [_,"Z"] = 3
+
+pointFromResult :: [String] -> Int
+pointFromResult ["A", "X"] = 3
+pointFromResult ["B", "Y"] = 3
+pointFromResult ["C", "Z"] = 3
+
+pointFromResult ["A", "Z"] = 0
+pointFromResult ["B", "X"] = 0
+pointFromResult ["C", "Y"] = 0
+
+pointFromResult ["A", "Y"] = 6
+pointFromResult ["B", "Z"] = 6
+pointFromResult ["C", "X"] = 6
+
+convertToRound :: [String] -> [String]
+convertToRound ["A", "X"] = ["A", "Z"]
+convertToRound ["A", "Y"] = ["A", "X"]
+convertToRound ["A", "Z"] = ["A", "Y"]
+
+convertToRound ["B", "X"] = ["B", "X"]
+convertToRound ["B", "Y"] = ["B", "Y"]
+convertToRound ["B", "Z"] = ["B", "Z"]
+
+convertToRound ["C", "X"] = ["C", "Y"]
+convertToRound ["C", "Y"] = ["C", "Z"]
+convertToRound ["C", "Z"] = ["C", "X"]
+
+pointFromRound :: [String] -> Int
+pointFromRound round = pointFromResult round + pointFromPick round
+
