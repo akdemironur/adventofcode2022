cdd252a72bfb49f7aea0b017e186a52a335e2928
Onur Akdemir <onur.akdemir@icloud.com>
2022-12-11 13:15:30 +0300

    Day 10 solution

---
+++ 10/solution.hs
@@ -0,0 +1,23 @@
+puzzleInput :: IO [String]
+puzzleInput = lines <$> readFile "input.txt"
+
+inputToInstr :: [Int] -> [String] -> [Int]
+inputToInstr xs [] = xs
+inputToInstr xs (str:ss)
+    | head str == 'n' = inputToInstr (xs ++ [0]) ss
+    | otherwise = inputToInstr (xs ++ [0,read $ drop 5 str]) ss
+
+allInstr = inputToInstr [] <$> puzzleInput
+
+xAfterInstr :: IO [Int]
+xAfterInstr = scanl (+) 1 <$> allInstr
+
+q1 = (\a -> sum $ zipWith (*) (fmap (a!!) [19,59..219]) [20,60..220]) <$> xAfterInstr
+
+cycleToChar spritePos cycle
+    | abs(cycle-spritePos) <= 1 = '#'
+    | otherwise = ' '
+
+q2 = (\a -> (zipWith cycleToChar a (concat . (take 6) $ repeat [0..39]))) <$> xAfterInstr
+
+main = q1 >>= print >> q2 >>= print
