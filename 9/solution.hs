174d0b134a70b079f7493b69928ad672787b4b7b
Onur Akdemir <onur.akdemir@icloud.com>
2022-12-11 13:16:39 +0300

    Day 9 update

--- 9/solution.hs
+++ 9/solution.hs
@@ -12,12 +12,7 @@ puzzleInput = fmap ((fmap ((\[a,b]->(a,read b)::(String,Int)) . words)).lines) (
 calcTailPos :: (Int,Int) -> (Int, Int) -> (Int, Int)
 calcTailPos (headX, headY) (tailX, tailY) 
     | abs(tailX-headX) <= 1 && abs(tailY-headY) <= 1 = (tailX, tailY)
-    | otherwise = (newDP headX tailX, newDP headY tailY)
-        where
-            newDP a b
-                | a > b = b+1
-                | a < b = b-1
-                | otherwise = b
+    | otherwise = (tailX + signum (headX - tailX), tailY + signum (headY - tailY))
 
 calcHeadPos :: String -> (Int, Int) -> (Int, Int)
 calcHeadPos "L" (headX, headY) = (headX-1, headY) 
@@ -26,17 +21,16 @@ calcHeadPos "U" (headX, headY) = (headX, headY+1)
 calcHeadPos "D" (headX, headY) = (headX, headY-1) 
 calcHeadPos _ _ = error "undefined direction" 
 
-initial :: Int -> ([(Int, Int)], [(Int, Int)])
-initial n = ([(0,0)], take n $ repeat (0,0))
+initialPos :: Int -> ([(Int, Int)], [(Int, Int)])
+initialPos n = ([(0,0)], take n $ repeat (0,0))
 
 moveIt :: ([(Int, Int)], [(Int,Int)]) -> (String, Int) -> ([(Int, Int)], [(Int,Int)])
 moveIt a (moveDir, 0) = a 
-moveIt (tailPosHistory,positions) (moveDir, moveMag) = moveIt (newTailPosHistory, newPoss) (moveDir, moveMag-1)
+moveIt (tailPosHistory, headPos:tailsPos) (moveDir, moveMag) = moveIt (newTailPosHistory, newPos) (moveDir, moveMag-1)
     where
-        newHeadPos = calcHeadPos moveDir (positions!!0)
-        newPoss = scanl calcTailPos newHeadPos (tail positions) 
-        tailPos = last newPoss
-        newTailPosHistory = if elem tailPos tailPosHistory then tailPosHistory else tailPos:tailPosHistory
+        newHeadPos = calcHeadPos moveDir headPos
+        newPos = scanl calcTailPos newHeadPos tailsPos
+        newTailPosHistory = if elem (last newPos) tailPosHistory then tailPosHistory else (last newPos):tailPosHistory
 
 numberOfTailVisitedLocations :: Int -> IO Int
-numberOfTailVisitedLocations n = length.fst.(foldl moveIt (initial n)) <$> puzzleInput
+numberOfTailVisitedLocations n = length.fst.(foldl moveIt (initialPos n)) <$> puzzleInput
